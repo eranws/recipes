3d4a3cfc44396b846e86aca0151c563dd830fef3
eranws <eranws@gmail.com>
2017-02-21 00:45:31 -0500

    improved version (drag&drop list of files into)

--- crop.py
+++ crop.py
@@ -1,16 +1,29 @@
+#!/usr/bin/python
+
+import sys
 import cv2
-fn="IMG_20160411_131757"
-ext=".jpg"
-inn=cv2.imread(fn + ext)
-w=1500
-h=w
-x=750
-y=100
 
-out=inn[x:x+w,y:y+h]
+def crop(fn):
+    inn=cv2.imread(fn)
+    w=1500
+    h=w
+    x=750
+    y=100
 
-# rotate cw
-out=cv2.transpose(out)
-out=cv2.flip(out,flipCode=1)
+    out=inn[x:x+w,y:y+h]
 
-cv2.imwrite(fn +"-crop.jpg", out)
+    # rotate cw
+    out=cv2.transpose(out)
+    out=cv2.flip(out,flipCode=1)
+
+    out=cv2.resize(out, (0,0), out, 0.5, 0.5)
+    
+    
+    cv2.imwrite(fn, out)
+
+def main(argv):
+    for s in argv:
+        crop(s)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
